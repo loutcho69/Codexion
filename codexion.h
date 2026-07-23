e8be638153e7babcfc6d3d64b8a605418d8ddff7
Lou Broue <loutcho69@MacBook-Pro-de-Lou.local>
2026-07-23 20:37:14 +0200

    time done

--- codexion.h
+++ codexion.h
@@ -61,6 +61,17 @@ typedef struct s_table
     pthread_mutex_t print_mutex;
     pthread_mutex_t stop_mutex;
 }   t_table;
+// --TIME-----------------------------------------
+struct timeval
+{
+    time_t      tv_sec;    // secondes depuis le 1er janvier 1970
+    suseconds_t tv_usec;   // microsecondes écoulées DANS la seconde courante
+};
+
+long get_time_ms(void);
+int  simulation_stopped(t_table *table);
+void log_state(t_table *table, int id, char *msg);
+void precise_usleep(long duration_ms, t_table *table);
 
 // -- Parsing ------------------------------
 int is_valid_number(char *str);
---
+++ time_utils.c
@@ -0,0 +1,38 @@
+#include "codexion.h"
+
+long get_time_ms(void)
+{
+    struct timeval tv;
+
+    gettimeofday(&tv, NULL);
+    return ( (long)tv.tv_sec * 1000 + tv.tv_usec/1000 );
+}
+
+int simulation_stopped(t_table *table)
+{
+    int ret;
+
+    pthread_mutex_lock(&table->stop_mutex);
+    ret = table->stop;
+    pthread_mutex_unlock(&table->stop_mutex);
+    return (ret);
+}
+void log_state(t_table *table, int id, char *msg)
+{
+    pthread_mutex_lock(&table->print_mutex);
+    if (!simulation_stopped(table))
+        printf("%ld %d %s\n", get_time_ms() - table->start_time, id, msg);
+    pthread_mutex_unlock(&table->print_mutex);
+}
+void precise_usleep(long duration_ms, t_table *table)
+{
+    long start;
+
+    start = get_time_ms();
+    while (get_time_ms() - start < duration_ms)
+    {
+        if (simulation_stopped(table))
+            break;
+        usleep(200);
+    }
+}
